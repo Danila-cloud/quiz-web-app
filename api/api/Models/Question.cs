25ffb349e66b6bdee8f15b100f781b85ac66027a
Danylo Provilskyi <danilprov2004@gmail.com>
2023-02-01 22:30:36 +0100

    feat: add migrations

--- api/api/Models/Question.cs
+++ api/api/Models/Question.cs
@@ -1,6 +1,30 @@
-﻿namespace api.Models;
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace api.Models;
 
 public class Question
 {
+    [Key]
+    public  int QnId { get; set; }
+
+    [Column(TypeName = "nvarchar(250")]
+    public string QnInWords { get; set; }
+
+    [Column(TypeName = "nvarchar(50")]
+    public string? ImageName { get; set; }
+
+    [Column(TypeName = "nvarchar(50")]
+    public string Option1 { get; set; }
     
+    [Column(TypeName = "nvarchar(50")]
+    public string Option2 { get; set; }
+    
+    [Column(TypeName = "nvarchar(50")]
+    public string Option3 { get; set; }
+    
+    [Column(TypeName = "nvarchar(50")]
+    public string Option4 { get; set; }
+
+    public int Answer { get; set; }
 }
