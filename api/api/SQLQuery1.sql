fd1574cc491afe0b09a5b07716aec79948f3a8a6
Danylo Provilskyi <danilprov2004@gmail.com>
2023-02-02 23:10:40 +0100

    feat: init data + build

---
+++ api/api/SQLQuery1.sql
@@ -0,0 +1,26 @@
+USE [QuizDB]
+GO
+SET IDENTITY_INSERT [dbo].[Questions] ON 
+GO
+INSERT [dbo].[Questions] ([QnId], [QnInWords], [ImageName], [Option1], [Option2], [Option3], [Option4], [Answer]) VALUES (1, N'What is a HTML?', N'html.png' , N'Hyper Trainer Marking Language', N'Hyper Text Marketing Language', N'Hyper Text Markup Language', N'Hyper Text Markup Leveler', 2)
+GO
+INSERT [dbo].[Questions] ([QnId], [QnInWords], [ImageName], [Option1], [Option2], [Option3], [Option4], [Answer]) VALUES (2, N'In C++ "string" is a', N'c-plus.png', N'Array', N'Variable', N'Number', N'Image', 2)
+GO
+INSERT [dbo].[Questions] ([QnId], [QnInWords], [ImageName], [Option1], [Option2], [Option3], [Option4], [Answer]) VALUES (3, N'What is npm?', N'npm.png', N'Variable', N'Language', N'Package manager', N'Class', 3)
+GO
+INSERT [dbo].[Questions] ([QnId], [QnInWords], [ImageName], [Option1], [Option2], [Option3], [Option4], [Answer]) VALUES (4, N'Which variant is mobile OS?', NULL, N'Android', N'Windows', N'MacOS', N'Linux', 1)
+GO
+INSERT [dbo].[Questions] ([QnId], [QnInWords], [ImageName], [Option1], [Option2], [Option3], [Option4], [Answer]) VALUES (5, N'Name the component?', N'cpu.png', N'RAM', N'Keyboard', N'GPU', N'GPU', 4)
+GO
+INSERT [dbo].[Questions] ([QnId], [QnInWords], [ImageName], [Option1], [Option2], [Option3], [Option4], [Answer]) VALUES (6, N' Which cryptocurrency has the largest supply?', N'crypto.png', N'Cardano', N'Bitcoin', N'Tenset', N'Ethereum', 2)
+GO
+INSERT [dbo].[Questions] ([QnId], [QnInWords], [ImageName], [Option1], [Option2], [Option3], [Option4], [Answer]) VALUES (7, N'Which programming language which is using for blockchain?', N'blockchain.png', N'C++', N'C#', N'Solidity', N'C', 3)
+GO
+INSERT [dbo].[Questions] ([QnId], [QnInWords], [ImageName], [Option1], [Option2], [Option3], [Option4], [Answer]) VALUES (8, N'What from this list is not a programming language?', N'programming.png', N'HTML', N'Rust', N'Ruby', N'TypeScript', 1)
+GO
+INSERT [dbo].[Questions] ([QnId], [QnInWords], [ImageName], [Option1], [Option2], [Option3], [Option4], [Answer]) VALUES (9, N'In which year was do first transaction in blockchain?', N'bitcoin.png', N'2007', N'2008', N'2009 NT', N'2016', 3)
+GO
+INSERT [dbo].[Questions] ([QnId], [QnInWords], [ImageName], [Option1], [Option2], [Option3], [Option4], [Answer]) VALUES (10, N'What from this list is not IDE?', N'ide.png', N'Notepad++', N'Rider', N'Visual studio', N'PyCharm', 1)
+GO
+SET IDENTITY_INSERT [dbo].[Questions] OFF
+GO
